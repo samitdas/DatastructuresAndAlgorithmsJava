35cc12c6ad58cdb2b72cbde42dedd458d0199d26
samitdas <dsamit@gmail.com>
2024-01-04 09:53:23 +0530

    dp using tabulation for coin change

--- src/main/java/org/samit/practice/dp/CoinChangeProblem.java
+++ src/main/java/org/samit/practice/dp/CoinChangeProblem.java
@@ -10,6 +10,7 @@ public class CoinChangeProblem {
         int sum = 4;
         CoinChangeProblem ccp = new CoinChangeProblem();
         System.out.println("Answer with recursive solution : " + ccp.coinChangeRecursive(coins, sum, coins.length));
+        System.out.println("Answer with dp solution : " + ccp.coinChangeDpSolution(coins, sum));
     }
 
     private int coinChangeRecursive(int[] coins, int sum, int i) {
@@ -22,4 +23,33 @@ public class CoinChangeProblem {
 
         return coinChangeRecursive(coins, sum, i - 1) + coinChangeRecursive(coins, sum - coins[i-1], i);
     }
+
+    /**
+     * Time : O(n*sum) , Aux space : O(n*sum)
+     * @param coins
+     * @param sum
+     * @return
+     */
+    private int coinChangeDpSolution(int[] coins, int sum){
+        int[][] tab = new int[coins.length + 1][sum+1];
+        int n = coins.length;
+        // init tab with base cases
+        // if sum = 0 , then tab[i][0] is 1
+        // if i = 0 and sum > 0 then tab[0][j] = 0
+        for(int i=0; i < n+1; i++ ){
+            tab[i][0] = 1;
+        }
+
+        for(int i = 1; i < n+1; i++){
+            for(int j=1; j < sum+1; j++){
+                if(coins[i-1] > j)
+                    tab[i][j] = tab[i-1][j];
+                else{
+                    tab[i][j] = tab[i-1][j] + tab[i][j-coins[i-1]];
+                }
+            }
+        }
+        return tab[n][sum];
+    }
+
 }
