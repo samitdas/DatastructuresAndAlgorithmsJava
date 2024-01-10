218381c7c616c4a45a545d96246cbefbbf5c4ce2
samitdas <dsamit@gmail.com>
2024-01-10 10:36:19 +0530

    simple binary search recursive

--- src/main/java/org/samit/practice/search/BinarySearch.java
+++ src/main/java/org/samit/practice/search/BinarySearch.java
@@ -10,11 +10,14 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50};
         BinarySearch bs = new BinarySearch();
-        System.out.println("index of 0 : " + bs.recursive(arr, 0));
-        System.out.println("index of 40 : " + bs.recursive(arr, 40));
+        System.out.println("iterative : index of 0 : " + bs.iterative(arr, 0));
+        System.out.println("iterative : index of 40 : " + bs.iterative(arr, 40));
+
+        System.out.println("recursive : index of 0 : " + bs.recursive(arr, 0, arr.length - 1, 0));
+        System.out.println("recursive : index of 40 : " + bs.recursive(arr, 0, arr.length - 1, 40));
     }
 
-    private int recursive(int[] arr, int val) {
+    private int iterative(int[] arr, int val) {
         // local var jdk11
         var l = 0;
         var h = arr.length - 1;
@@ -29,5 +32,16 @@ public class BinarySearch {
         }
 
         return (h < 0 || l > h) ? -1 : l;
+    }
+
+    private int recursive(int[] arr, int l, int h, int val) {
+        if (h < 0) return -1;
+        if (l > h) return -1;
+
+        var mid = (l + h) / 2;
+
+        if (arr[mid] == val) return mid;
+        else if (arr[mid] < val) return recursive(arr, mid + 1, h, val);
+        else return recursive(arr, l, mid - 1, val);
     }
 }
